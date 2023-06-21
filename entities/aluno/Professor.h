68b3f6e20a79f50bab3c8fc93591c1be5cfee208
MikaelMagal <mikaelmagalhaes28@gmail.com>
2023-06-21 16:57:49 -0300

    FIX: foi consertado erro do input ao ler nome do aluno e criado a entidade Gestor e Professor com alguns nomes de funções.

---
+++ entities/aluno/Professor.h
@@ -0,0 +1,34 @@
+//
+// Created by Mikael on 21/06/2023.
+//
+
+#ifndef TP01_AEDS2_CPP_PROFESSOR_H
+#define TP01_AEDS2_CPP_PROFESSOR_H
+
+#include <string>
+class Professor {
+    public:
+        int id;
+        char nome[50];
+        char data_nascimento[11];
+        int telefone;
+};
+
+//Imprimir os dados do professor
+void imprime_professor(Professor *professor);
+
+//Cria professor
+Professor cria_professor(int id, const char *nome, const char *data_nascimento, int telefone);
+
+void salva_professor (Professor *professor, FILE *out);
+
+// Le um professor do arquivo in na posicao atual do cursor
+// Retorna um ponteiro para professor lido do arquivo
+Professor *le_professor(FILE *in);
+
+//// Retorna tamanho do professor em bytes
+int tamanho_prof();
+
+Professor* busca_id_professor(int id, FILE *arq, int tam);
+
+#endif //TP01_AEDS2_CPP_PROFESSOR_H
